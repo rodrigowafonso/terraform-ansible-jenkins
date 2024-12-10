ad332a0f0c10a8f5251e26049e7a2cd731c49357
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-10 14:43:39 -0300

    Criando a infrestrutura VPC do Projeto

--- vpc.tf
+++ vpc.tf
@@ -27,11 +27,6 @@ resource "aws_internet_gateway" "internet_gateway_rwa_jta" {
     } 
 }
 
-resource "aws_internet_gateway_attachment" "associa_gw_vpc" {
-    internet_gateway_id = aws_internet_gateway.internet_gateway_rwa_jta.id
-    vpc_id = aws_vpc.vpc_rwa_jta.id  
-}
-
 resource "aws_route_table" "tabela_rota_rwa_jta" {
     vpc_id = aws_vpc.vpc_rwa_jta.id
 
@@ -45,4 +40,10 @@ resource "aws_route_table" "tabela_rota_rwa_jta" {
       Env = "Dev"
     }
 
+}
+
+resource "aws_route_table_association" "associa_rt_gw_rwa_jta" {
+    subnet_id = aws_subnet.subnet_rwa_jta.id
+    route_table_id = aws_route_table.tabela_rota_rwa_jta.id
+  
 }
