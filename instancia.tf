ad4885bc967711c978e08623899f33d2c04fab1c
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-10 15:08:03 -0300

    Criando a infrestrutura VPC do Projeto

--- instancia.tf
+++ instancia.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "srv_webserver_rwa" {
     instance_type = "t2.micro"
     key_name = data.aws_key_pair.key_devops_rwa.key_name
     subnet_id = aws_subnet.subnet_rwa_jta.id
-
+    vpc_security_group_ids = [aws_security_group.security_gropu_rwa_jta.id]
 
     tags = {
         Name = "srv_webserver_rwa"
---
+++ securitygroup.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "security_gropu_rwa_jta" {
+    name = "security_gropu_rwa_jta"
+    vpc_id = aws_vpc.vpc_rwa_jta.id
+
+    tags = {
+      Name = "security_gropu_rwa_jta"
+      Env = "Dev"
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+  
+
+}
