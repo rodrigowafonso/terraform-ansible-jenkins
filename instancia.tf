4a16827a66950a4b1c656ff73bf6077fda8a101b
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-10 11:39:29 -0300

    Integrando Terraform com Jenkins

--- instancia.tf
+++ instancia.tf
@@ -6,7 +6,7 @@ data "aws_key_pair" "key_devops_rwa" {
 resource "aws_instance" "srv_webserver_rwa" {
     ami = "ami-0e2c8caa4b6378d8c"
     instance_type = "t2.micro"
-    key_name = aws_key_pair.key_devops_rwa.Name
+    key_name = data.aws_key_pair.key_devops_rwa.key_name
 
     tags = {
         Name = "srv_webserver_rwa"
