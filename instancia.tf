4717d5e0fe3252903235d29c06faaa45a354bcc2
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-10 14:52:37 -0300

    Criando a infrestrutura VPC do Projeto

--- instancia.tf
+++ instancia.tf
@@ -7,6 +7,8 @@ resource "aws_instance" "srv_webserver_rwa" {
     ami = "ami-0e2c8caa4b6378d8c"
     instance_type = "t2.micro"
     key_name = data.aws_key_pair.key_devops_rwa.key_name
+    subnet_id = aws_subnet.subnet_rwa_jta.id
+
 
     tags = {
         Name = "srv_webserver_rwa"
