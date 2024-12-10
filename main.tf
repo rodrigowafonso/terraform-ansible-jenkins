e4611d6d5eec0eb93fadd212dd515b706b1bdce6
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-10 12:17:06 -0300

    Integrando Terraform com Jenkins

--- main.tf
+++ main.tf
@@ -10,3 +10,8 @@ terraform {
     }
   }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.regiao_projeto
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "regiao_projeto" {
+    description = "Define a região do Projeto"
+    type = string
+    default = "us-east-1"
+}
