eeffa64ff2279297243ed97c803738672dd6bc0e
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-11 15:06:41 -0300

    Atualizando o código da infra Terraform e Ansible

--- iam-role.tf
+++ iam-role.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "aws_access_rwa_jta" {
     name = "${var.prefix}_role"
-    assume_role_policy =  jsondecode({
+    assume_role_policy =  jsonencode({
         Version = "2012-10-17"
         Statement = [
             {
