7b9f311f1d92ad3cba9a1d38e838ba00a8a92696
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-12 14:50:22 -0300

    Criando a política role iam EC2 ContainerRegistry

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-# output "instance_public_ips" {
-#   value = { for i in aws_instance.aws_instance.srv_webserver_rwa : i.tags["Name"] => i.public_ip }
-# }
+output "{var.instancia_nome[0]}-rwa-jta" {
+    value = aws_instance.srv_webserver_rwa[0].public_ip
+}
+
+output "{var.instancia_nome[1]}-rwa-jta" {
+    value = aws_instance.srv_webserver_rwa[1].public_ip
+}
+
+output "{var.instancia_nome[2]}-rwa-jta" {
+    value = aws_instance.srv_webserver_rwa[2].public_ip
+}
