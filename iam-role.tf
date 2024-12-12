a499e219aa793fca11f34042b3c4d3e52875292a
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-12 14:38:56 -0300

    Criando a política role iam EC2 ContainerRegistry

--- iam-role.tf
+++ iam-role.tf
@@ -20,3 +20,8 @@ resource "aws_iam_role_policy_attachment" "anexar_politica_rwa_jta" {
     role = aws_iam_role.criar_role_rwa_jta.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
+
+resource "aws_iam_instance_profile" "profile_instacia_rwa_jta" {
+    name = "${var.prefix}-profile"
+    role = aws_iam_role.criar_role_rwa_jta.name
+}
