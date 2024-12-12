d5f70f2d0693faff1e68fdddfcb6cb3286459379
rodrigowafonso <rodrigowalterafonso@gmail.com>
2024-12-12 09:27:22 -0300

    Atualizando o código da infra Terraform e Ansible

--- securitygroup.tf
+++ securitygroup.tf
@@ -16,6 +16,14 @@ resource "aws_security_group" "security_gropu_rwa_jta" {
         ipv6_cidr_blocks = ["::/0"]
     }
 
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
